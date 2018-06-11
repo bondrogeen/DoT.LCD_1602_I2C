f9846fd541aadbe6c4c22f067b3cd49ac528f666
bondrogeen <bondrogeen@gmail.com>
2018-06-11 11:45:08 +0300

    fix minor

--- files/LCD_1602_I2C.lua
+++ files/LCD_1602_I2C.lua
@@ -16,6 +16,7 @@ local function send(d,r)
 end
 local function init(t)
  if not _I2C then i2c.setup(id,t.sda,t.scl,i2c.SLOW)end
+ _WH1602={}
  send({0x30})
  tmr.delay(4100)
  send({0x30})
@@ -24,23 +25,27 @@ local function init(t)
  send({0x20,0x20,0x80})
  send({0x00,0x10})
  send({0x00,0xc0})
+ return true
 end
-local function cursor(op)if(op==1)then send({0x00,0xe0})else send({0x00,0xc0})end end
-local function cls()send({0x00,0x10})end
-local function home()send({0x00,0x20})end
+local function cursor(op)if(op==1)then send({0x00,0xe0})else send({0x00,0xc0})end return true end
+local function cls()send({0x00,0x10}) return true end
+local function home()send({0x00,0x20}) return true end
 local function lcd(t)
  if (t.line==2)then send({0xc0,bit.lshift(t.col,4)})elseif(t.line==1)then send({0x80,bit.lshift(t.col,4)})end
 for i=1,#t.str do
  local char=string.byte(string.sub(t.str,i,i))
  send({bit.clear(char,0,1,2,3),bit.lshift(bit.clear(char,4,5,6,7),4)},1)
  end
+ return true
 end
 return function(t)
-local r
+local r=false
 if t.init then r=init(t.init)end
-if t.cls then r=cls()end
-if t.home then r=home()end
-if t.cursor then r=cursor(t.cursor)end
-if t.lcd then r=lcd(t.lcd)end
+ if _WH1602 then
+  if t.cls then r=cls()end
+  if t.home then r=home()end
+  if t.cursor then r=cursor(t.cursor)end
+  if t.lcd then r=lcd(t.lcd)end
+ end
 return r
 end
